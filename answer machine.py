c5a05a50e54a41243d095e97ec1115bcf48d9417
David Sanchez Hidalgo <32333682+daviddsdp@users.noreply.github.com>
2019-10-15 13:14:32 +0200

    Update and rename never remove this one version.py to answer machine.py

--- answer machine.py
+++ answer machine.py
@@ -6,57 +6,38 @@ def replace_all(repls, str):
                  lambda k: repls[k.group(0)], str) 
 
 
-text = input (" Ask me something to do: ") 
+text = input("Ask me for:") 
 
 
 if  re.search(" enjoy | fun ", text):
-    x = 1
+    z =  1/3
 elif re.search(" injure | pitiful ", text):
-     x= -1
+    z = -1/3
 else:
-    x= 0
-
-text1 = input ("Tell me more about it:") 
+    z =  0
 
 
-if re.search(" enjoy | fun ", text1):
-   x1 = 1
-elif re.search(" injure | pitiful ", text1):
-     x1= -1
-else:
-    x1= 0
-
-
-text2 = input ("Tell me more about it:") 
-
-
-if re.search(" enjoy | fun ", text2):
-   x2= 1
-elif re.search(" injure | pitiful ", text2):
-     x2= -1
-else:
-   x2= 0
 
     
-if x + x1 + x2 > 1:
-   print ("Yes,",replace_all({" i ": "you", "you": "i", "am": "are",
+if   z >=  1/3:
+    print ("Yes,",replace_all({" i ": "you", "you": "i", "am": "are",
    "are": "am", "my":"your", "your":"my", "with you":"with me",
    "if": " ", "with me": "with you", "i was": "you were"}, text))
 
-elif -1 > x + x1 +x2:
+elif z <= -1/3:
      print ("I not think so that,",replace_all({" i ": "you", "you": "i", "am": "are",
      "are": "am", "my":"your", "your":"my", "with you":"with me",
      "if": " ", "with me": "with you", "i was": "you were"}, text))
 
-elif x + x1 + x2 > -1 and  x + x1 + x2 <1  :
+elif z > -1/3  and  z <1/3:
      print ("Maybe,",replace_all({" i ": "you", "you": "i", "am": "are",
      "are": "am", "my":"your", "your":"my", "with you":"with me",
      "if": " ", "with me": "with you", "i was": "you were"}, text))
 
-elif x + x1 + x2 == 0 :  
- print ("I don't know if,",replace_all({" i ": "you", "you": "i", "am": "are",
- "are": "am", "my":"your", "your":"my", "with you":"with me",
-"if": " ", "with me": "with you", "i was": "you were"}, text))
+elif z == 0:  
+     print ("I don't know if,",replace_all({" i ": "you", "you": "i", "am": "are",
+     "are": "am", "my":"your", "your":"my", "with you":"with me",
+     "if": " ", "with me": "with you", "i was": "you were"}, text))
     
   
      
