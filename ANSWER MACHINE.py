3158991983864fcf995663fefe36dc3516d6b722
David Sanchez Hidalgo <32333682+daviddsdp@users.noreply.github.com>
2019-10-15 13:28:18 +0200

    Update ANSWER MACHINE.py

--- ANSWER MACHINE.py
+++ ANSWER MACHINE.py
@@ -1,5 +1,3 @@
-import re
-
 def replace_all(repls, str):
 
     return re.sub('|'.join(re.escape(key) for key in repls.keys()),
@@ -9,27 +7,27 @@ def replace_all(repls, str):
 text = input("Ask me for:") 
 
 
-if  re.search(" enjoy | fun ", text):
-    z =  1/3
-elif re.search(" injure | pitiful ", text):
-    z = -1/3
+if   re.findall("enjoy|fun|good", text):
+     z =  1/3
+elif re.findall("injure|pitiful|bad", text):
+     z = -1/3
 else:
-    z =  0
+     z =  0
 
 
 
     
-if   z >=  1/3:
+if   z >=  1:
     print ("Yes,",replace_all({" i ": "you", "you": "i", "am": "are",
-   "are": "am", "my":"your", "your":"my", "with you":"with me",
-   "if": " ", "with me": "with you", "i was": "you were"}, text))
+    "are": "am", "my":"your", "your":"my", "with you":"with me",
+    "if": " ", "with me": "with you", "i was": "you were"}, text))
 
-elif z <= -1/3:
+elif z <= -1:
      print ("I not think so that,",replace_all({" i ": "you", "you": "i", "am": "are",
      "are": "am", "my":"your", "your":"my", "with you":"with me",
      "if": " ", "with me": "with you", "i was": "you were"}, text))
 
-elif z > -1/3  and  z <1/3:
+elif z > -1  and  z <1:
      print ("Maybe,",replace_all({" i ": "you", "you": "i", "am": "are",
      "are": "am", "my":"your", "your":"my", "with you":"with me",
      "if": " ", "with me": "with you", "i was": "you were"}, text))
@@ -38,11 +36,3 @@ elif z == 0:
      print ("I don't know if,",replace_all({" i ": "you", "you": "i", "am": "are",
      "are": "am", "my":"your", "your":"my", "with you":"with me",
      "if": " ", "with me": "with you", "i was": "you were"}, text))
-    
-  
-     
-
-     
-   
-
-					
