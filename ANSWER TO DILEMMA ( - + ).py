2471a50179e84388fca7222f27eb537d800e7a17
David Sanchez Hidalgo <32333682+daviddsdp@users.noreply.github.com>
2019-10-17 16:27:28 +0200

    Add files via upload

---
+++ ANSWER TO DILEMMA ( - + ).py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import re
+
+
+a = input('+ values to dilemma:')
+b = input('- values to dilemma:')
+
+    
+
+while True:
+    b = input('Ask me for the dilemma:') # Typ one word per input
+    if re.findall(b,a,re.VERBOSE):
+        (z)= 1
+    else:
+        (z)= 0 
+    if re.findall(b,a,re.VERBOSE):
+        (z1)= 1
+    else:
+        (z1)= -1     
+
+    if   0<(z)+(z1):
+        print('Maybe yes!')
+    elif 0>(z)+(z1):
+        print('Maybe not!')
+    else:
+        print('Not idea.')
+
+
+# In[ ]:
+
+
+
+
