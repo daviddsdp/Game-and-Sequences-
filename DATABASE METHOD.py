03ee987c1507b68920c4b73ad5d9455650b8dd0b
David Sanchez Hidalgo <32333682+daviddsdp@users.noreply.github.com>
2019-10-27 21:23:16 +0100

    Add files via upload

---
+++ DATABASE METHOD.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import re 
+
+
+
+a = input('Paste hier your enconde fun. Then where and when you offer us:')
+    
+b = re.sub(" ",",", a)
+
+print(b)
+
+
+# In[ ]:
+
+
+
+
