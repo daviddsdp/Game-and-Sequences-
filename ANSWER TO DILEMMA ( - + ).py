35194a462bed1957ab731af31fc060bcaf4c3f5c
David Sanchez Hidalgo <32333682+daviddsdp@users.noreply.github.com>
2019-10-27 23:19:58 +0100

    Update ANSWER TO DILEMMA ( - + ).py

--- ANSWER TO DILEMMA ( - + ).py
+++ ANSWER TO DILEMMA ( - + ).py
@@ -1,37 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 import re
 
 
-a = re.sub(" ",",",input('+ values to dilemma:')) # You can typ your values system as sentences
-b = re.sub(" ",",",input('- values to dilemma:'))
+a = re.sub(" ",",", input('+ values to dilemma:')) # You can typ your values system as sentences
+b = re.sub(" ",",", input('- values to dilemma:'))
 
+print(a,b)
     
 
 while True:
-    c = input('Ask me for the dilemma:') # Typ one word per input
-    if re.findall(c,a,re.VERBOSE):
-        (z)= 1
+    c = re.sub(" ",",", input('Ask me for the dilemma:'))
+    print(c)
+    if re.search(c,a):
+        z=   1
     else:
-        (z)= 0 
-    if re.findall(c,b,re.VERBOSE):
-        (z1)= 1
+        z= 0 
+    if re.search(c,b):
+        z1= -1
     else:
-        (z1)= -1     
+        z1= 0     
 
-    if   0<(z)+(z1):
+    if  (z)+(z1)>0:
         print('Maybe yes!')
     elif 0>(z)+(z1):
         print('Maybe not!')
     elif 0==(z)+(z1):
         print('Not idea!')
-
-
-# In[ ]:
-
-
-
-
