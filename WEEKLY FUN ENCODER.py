1ab8fdbf3933db09ad3535692532e7d02432bb9d
David Sanchez Hidalgo <32333682+daviddsdp@users.noreply.github.com>
2019-10-27 21:26:09 +0100

    Update WEEKLY FUN ENCODER.py

--- WEEKLY FUN ENCODER.py
+++ WEEKLY FUN ENCODER.py
@@ -13,15 +13,10 @@ a = input('Paste hier your enconde fun. Then where and when you offer us:')
 while True:
     b = input('Tell me please about your fun. Then when and where your looking for:')
     if re.findall(b,a,re.VERBOSE):
-        (z)= 1
-    else:
-        (z)= 0 
-
-    if 0<(z):
         print('Something happen let you know!')
-    
     else:
-        print('Notting happen around the world.')
+        print('Notting happen around the world.') 
+
 
 
 # In[ ]:
