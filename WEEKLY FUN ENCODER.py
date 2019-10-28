8fd7a10b76aaaefdf4959bd2fef2c7b7ec592b5e
David Sanchez Hidalgo <32333682+daviddsdp@users.noreply.github.com>
2019-10-28 15:35:44 +0100

    Update WEEKLY FUN ENCODER.py

--- WEEKLY FUN ENCODER.py
+++ WEEKLY FUN ENCODER.py
@@ -5,7 +5,7 @@ a = re.sub(" ","",input('Paste hier your enconded fun. Then where and when you o
     
 
 while True:
-    b = input('Tell me please about your fun. Then when and where your looking for. One word per input is anought')) 
+    b = input('Tell me please about your fun. Then when and where youre looking for. One word per input is enough')) 
     if  re.search(b,a):
         print('Something happen let you know!')
     else:
