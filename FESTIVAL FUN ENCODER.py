0b83b624a0c36280b238651602ada9c41b81ada5
David Sanchez Hidalgo <32333682+daviddsdp@users.noreply.github.com>
2019-10-28 16:04:27 +0100

    Update FESTIVAL FUN ENCODER.py

--- FESTIVAL FUN ENCODER.py
+++ FESTIVAL FUN ENCODER.py
@@ -2,17 +2,16 @@ import re
 
 
 a = re.sub(" ","|",input('Typ hier as sentences your enconded fun. Then where and when you offer it us:'))
-b = re.sub(".","|",a)
-print(b)
+
+print(a)
     
 
 while True:
-    c = input('Tell me please about your fun. Then when and where youre looking for. One word per input is enough')
-    if  re.search(c,b):
+    b = input('Tell me please about your fun. Then when and where youre looking for. One word per input is enough:')
+    if  re.search(a,b):
         print('Something happen let you know!')
     else:
         print('Notting happen around the world.') 
-
 
 
 
